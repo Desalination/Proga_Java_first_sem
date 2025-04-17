10c8b348abbce50b6fbc00b1c08baa8cd6e72007
Denis <79279302+Desalination@users.noreply.github.com>
2025-04-17 03:07:02 +0300

    Delete Lab_5 directory

--- Lab_5/src/com/Lab_5/ClientProject/Support/ExecuteScriptValidation.java
+++
@@ -1,8 +0,0 @@
-package com.Lab_5.ClientProject.Support;
-
-import java.util.LinkedList;
-
-public class ExecuteScriptValidation {
-    static LinkedList<String> scriptAllCommands;
-
-}
--- Lab_5/src/com/Lab_5/ServerProject/Commands/CommandManager.java
+++
@@ -1,5 +0,0 @@
-package com.Lab_5.ServerProject.Commands;
-
-public interface CommandManager {
-    void execute(Command command);
-}
